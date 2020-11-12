18225880db60561ce49a0f01b2fa407c41c89ce5
Yevjenii-Lim <70367939+Yevjenii-Lim@users.noreply.github.com>
2020-11-12 17:41:09 +0200

    Add files via upload

--- slide.js
+++ slide.js
@@ -8,8 +8,13 @@ let num = img.offsetWidth
 let dot = document.querySelectorAll('.container__slider__dots__dot')
 let timer
 let count = 0;
+let range = document.querySelector('#range')
+let karusel = document.querySelector('.container__karusel')
+let menu = document.querySelector('.container__logo__menu')
+let fullSlideMenu = document.querySelector('.container__logo__menu__ul')
+let logo = document.querySelector('.container__logo')
 function autoSlide(){
-    timer = setTimeout(slide, 3000)
+    timer = setTimeout(slide, 3500)
 }
 autoSlide()
 function slide() { 
@@ -44,4 +49,30 @@ line.addEventListener('click', stopSlide)
 
 function stopSlide() {
     clearTimeout(timer)
+}
+
+range.addEventListener('input', rangeSlide)
+range.max =  karusel.offsetWidth - 900
+range.value = (karusel.offsetWidth - 900) / 2
+function rangeSlide(event){
+    
+    karusel.style.left = -event.target.value + 'px'
+    
+   
+}
+
+
+menu.addEventListener('mouseover', fullMenu)
+
+function fullMenu () {
+    let slideMenu = document.querySelector('.container__logo__menu__ul')
+    slideMenu.style.transform = 'scaleY(1)'
+    logo.style.background = '#3B3B3B'
+}
+
+fullSlideMenu.addEventListener('mouseout', hideMenu)
+
+function hideMenu() {
+    let slideMenu = document.querySelector('.container__logo__menu__ul')
+    slideMenu.style.transform = 'scaleY(0)'
 }
